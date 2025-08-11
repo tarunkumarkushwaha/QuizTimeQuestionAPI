fc43a14c99eb2f3e514857dc4d5f7b8b2f01476f
tarun <tkk.tarunkumar02@gmail.com>
2025-08-11 17:37:37 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'))      // to use files publicly
